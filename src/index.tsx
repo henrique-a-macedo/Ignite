8983a5ebdebe42e81349f151f2a0194219634420
Henrique <henrique.macedo@etools.com.br>
2022-10-20 21:24:35 -0300

    config typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App'
 
 const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App tab="home"/>)
+const root = createRoot(container!)
+root.render(<App/>)
 
